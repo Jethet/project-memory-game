e0b9279dcf97a20490d2adbcf0995bfd6e17d049
Jethet <35808885+Jethet@users.noreply.github.com>
2020-04-21 10:06:41 +0200

    Create algorithms.js

---
+++ algorithms.js
@@ -0,0 +1,13 @@
+function getIndexToIns(arr, num) {
+  let sortArr = arr.sort(function(a, b){return a-b})
+  console.log(sortArr)
+  let newArr = []
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] < num) {
+      newArr.push(arr[i])
+    } else {
+    num = newArr.length
+  }
+  }
+  return num;
+}
