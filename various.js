6d89b77096d442b8149c25c038d9d3837fdd21ed
Jethet <35808885+Jethet@users.noreply.github.com>
2020-04-21 18:41:14 +0200

    Update various.js

--- various.js
+++ various.js
@@ -35,3 +35,19 @@ function formatTime(num) {
     }, 1000);
   }
 }
+
+
+// Add delay to loop for making requests to API
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+const list = [1, 2, 3, 4]
+const doSomething = async () => {
+  for (const item of list) {
+    await sleep(1000)
+    console.log('🦄')    
+  }
+}
+
+doSomething()
