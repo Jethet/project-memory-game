9ace3db6faa1846d8e78150e0b7dc98e6aa5efa6
Jethet <35808885+Jethet@users.noreply.github.com>
2020-04-21 10:10:27 +0200

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -2,7 +2,7 @@ function getIndexToIns(arr, num) {
   let sortArr = arr.sort(function(a, b){return a-b})
   console.log(sortArr)
   let newArr = []
-  for(let i = 0; i < arr.length; i++) {
+  for(let i = 0; i <= arr.length; i++) {
     if (arr[i] < num) {
       newArr.push(arr[i])
     } else {
