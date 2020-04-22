205906fbf1b889c3fa4e5172722de726d14f47ac
Jethet <35808885+Jethet@users.noreply.github.com>
2020-04-22 08:55:47 +0200

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -18,3 +18,19 @@ function getIndexToIns(arr, num) {
 if (value) {
   // do something
   }
+
+// Dividing arrays: the Array instance splice() method:
+const list = [1, 2, 3, 4, 5, 6]
+const half = Math.ceil(list.length / 2);
+
+const firstHalf = list.splice(0, half)
+const secondHalf = list.splice(-half)
+
+
+//Remove LAST char of string: (slice does not alter original array, create new)
+const text = 'abcdef'
+const editedText = text.slice(0, -1) //'abcde'
+
+//Remove FIRST char of string:
+const text = 'abcdef'
+const editedText = text.slice(1) //'bcdef'
