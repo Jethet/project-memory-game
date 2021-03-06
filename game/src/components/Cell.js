8377787d93ae7420df9311ab1b8770b0366ddaa2
Jethet <henriette.hettinga@protonmail.com>
2021-03-06 14:45:46 +0100

    Create Cell component, add styling

---
+++ game/src/components/Cell.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState, useEffect } from "react"
+
+function Cell() {
+
+  const [color, setColor] = useState(null)
+
+
+
+  return (
+    <div className="grid-cell">
+    Cell
+    </div>
+  );
+}
+
+export default Cell;
--- game/src/components/Grid.js
+++ game/src/components/Grid.js
@@ -1,18 +1,35 @@
 import React from "react";
+import Cell from "./Cell";
 
 function Grid() {
   return (
     <div className="grid-container">
-    <div className="grid">
-      <div>One</div>
-      <div>Two</div>
-      <div>Three</div>
-      <div>Four</div>
-      <div>Five</div>
-      <div>Six</div>
-      <div>Seven</div>
-      <div>Eight</div>
-      <div>Nine</div>
+      <div className="grid">
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
       </div>
     </div>
   );
