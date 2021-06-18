2c18e5b093aa151539942b504ee64ae311ce6dea
Jethet <henriette.hettinga@protonmail.com>
2021-06-18 15:28:26 +0200

    Redo export buttoncontainer

--- game/src/components/ButtonContainer.js
+++ game/src/components/ButtonContainer.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 function ButtonContainer(props) {
-
   const handleClick = () => {
-    props.selectRandom(7)
-  }
+    let randomCells = props.selectRandom(7);
+    props.colorCells(randomCells);
+  };
 
   return (
     <div className="button-container">
-        <button className="button-play" onClick={handleClick}>PLAY!</button>
-        <button className="button-clear">CLEAR</button>
-      </div>
-  )
+      <button className="button-play" onClick={handleClick}>
+        PLAY!
+      </button>
+      <button className="button-clear" onClick={props.resetGrid}>
+        CLEAR
+      </button>
+    </div>
+  );
 }
 
-export default ButtonContainer
+export default ButtonContainer;
--- game/src/components/Grid.js
+++ game/src/components/Grid.js
@@ -1,70 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cell from "./Cell";
 import ButtonContainer from "./ButtonContainer";
 
 function Grid() {
-  const cellsArray = [
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-    "white",
-  ];
   const [color, setColor] = useState("white");
+  
+  const cellsArray = [];
 
-  const colorCells = () => {
-    if (color === "white") {
-      setColor("blue")
-    }
-  }
-
-  const resetGrid = () => {
-    if (color === "blue") {
-      setColor("white");
-    }
-  }
+  for (let i = 0; i < 25; i++) {
+    cellsArray.push(i);
+  }  
 
   function selectRandom(number) {
-    let randomIndexes = []
-
+    let randomIndexes = [];
     while (randomIndexes.length < number) {
-      let randomIndex = Math.floor(Math.random() * cellsArray.length)
-  
+      let randomIndex = Math.floor(Math.random() * cellsArray.length);
       if (!randomIndexes.includes(randomIndex)) {
-        randomIndexes.push(randomIndex)
+        randomIndexes.push(randomIndex);
       }
     }
     console.log(randomIndexes);
-  
-}
+    }
+
+  // useEffect(() => {
+  // }, [color])
+
+
+  const colorCells = () => {
+    setColor("blue");
+  };
+
+  console.log("Test", color);
+
+  const resetGrid = () => {
+      setColor("white");
+  };
+
   return (
     <div className="wrapper">
       <div className="grid-container">
         <div className="grid">
           {cellsArray.map((item, index) => {
-            return <Cell key={index} color={item} id={index} />;
+            return <Cell key={index} color={color} id={item} />;
           })}
         </div>
       </div>
-      <div>
-        <ButtonContainer selectRandom={selectRandom}/>
+        <ButtonContainer selectRandom={selectRandom} resetGrid={resetGrid} colorCells={colorCells} />
       </div>
-    </div>
   );
 }
 
