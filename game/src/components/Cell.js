601b2a9a58c2dd5782b21f2e8df30b44f050ad94
Jethet <henriette.hettinga@protonmail.com>
2021-07-09 15:44:43 +0200

    Add show solution and reset border plus test

--- game/src/components/Cell.js
+++ game/src/components/Cell.js
@@ -4,11 +4,11 @@ function Cell(props) {
   const [border, setBorder] = useState("1px solid grey");
 
   const handleClick = () => {
-      // this checks if a cell id is in the random array
+    // this checks if a cell id is in the random array
     if (props.correctChoices.includes(props.id)) {
       // if the cell is in the random array, it has to change color to green when clicked
       props.colorCells([props.id], "rgb(39, 108, 76)");
-      props.setCount(props.count + 1)
+      props.setCount(props.count + 1);
     } else {
       props.colorCells([props.id], "rgb(169, 49, 44)");
     }
@@ -21,18 +21,17 @@ function Cell(props) {
   };
 
   const showSolution = () => {
-    props.correctChoices.forEach(cell => setBorder([cell],"3px solid yellow"))
-
-    // props.correctChoices.forEach((id) => {
-    //   // props.setBorder([id.border], "3px solid yellow");
-    //   console.log(props.color, props.id);
-    // })
-
-    // setTimeout(resetBorder(), 3000)
+    let solutionArray = [];
+    props.correctChoices.forEach((item) => {
+      solutionArray.push({ key: item, border: border });
+      return solutionArray;
+    });
+    setBorder("3px solid yellow")
+    console.log(solutionArray);
   };
 
   const resetBorder = () => {
-    props.setBorder({ border: "1px solid lightgrey" });
+    setTimeout(setBorder("1px solid lightgrey"), 5000)
   };
 
   return (
